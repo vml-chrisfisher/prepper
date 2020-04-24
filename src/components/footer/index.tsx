70bdb347a62f36df4d90fe35a9f30abead5ad9c7
Chris Fisher <chris.fisher@vml.com>
2020-04-23 21:22:35 -0500

    Styled footer links.

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,36 +4,64 @@ import * as baseStyles from '../base.module.css';
 import * as styles from './styles.module.css';
 
 const Footer = () => (
-  <div className={baseStyles.row} style={{paddingBottom: '100px'}}>
-    <div className={baseStyles.col3}>
-     
+  <div>
+    <div className={baseStyles.row} style={{paddingBottom: '175px'}}>
+      <div className={baseStyles.col3} />
+      <div className={baseStyles.col3}>
+          <div className={styles.footerTitle}>Shop</div>
+          <div className={styles.footerParagraph}>
+            Sign up to our newsletter and get exclusive. Seedlip news, updates and offers.
+          </div>
+          <a href="" className={styles.footerLinkMain}>
+            Shop Now
+          </a>
+      </div>
+      <div className={baseStyles.col3}>
+          <div className={styles.footerTitle}>Recipes</div>
+          <div className={styles.footerParagraph}>
+            Sign up to our newsletter and get exclusive. Seedlip news, updates and offers.
+          </div>
+          <a href="" className={styles.footerLinkMain}>
+            Get Recipes
+          </a>
+      </div>
+      <div className={baseStyles.col3}>
+          <div className={styles.footerTitle}>Story</div>
+          <div className={styles.footerParagraph}>
+            Sign up to our newsletter and get exclusive. Seedlip news, updates and offers.
+          </div>
+          <a href="" className={styles.footerLinkMain}>
+            Read
+          </a>
+      </div>
     </div>
-    <div className={baseStyles.col3}>
-        <div className={styles.footerTitle}>Shop</div>
-        <div className={styles.footerParagraph}>
-          Sign up to our newsletter and get exclusive. Seedlip news, updates and offers.
+    <div className={baseStyles.row} style={{ paddingBottom: '100px' }}>
+      <div className={baseStyles.col3} />
+      <div className={baseStyles.col9}>
+        <div className={baseStyles.col4}>
+          <a href="">
+            <img className={styles.footerSocialIcon} src={'/facebook.svg'} />
+          </a>
+          <a href="">
+            <img className={styles.footerSocialIcon} src={'/instagram.svg'} />
+          </a>
+          <a href="">
+            <img className={styles.footerSocialIcon} src={'/pinterest.svg'} />
+          </a>
         </div>
-        <div className={styles.footerLinkMain}>
-          Shop Now
+        <div className={baseStyles.col8}>
+          <div className={styles.footerBottomLinkContainer}>
+            <a href="" className={styles.footerLinkBottom}>Plants</a>
+            <a href="" className={styles.footerLinkBottom}>Recipes</a>
+            <a href="" className={styles.footerLinkBottom}>Contact Us</a>
+            <a href="" className={styles.footerLinkBottom}>Shipping</a>
+            <a href="" className={styles.footerLinkBottom}>Returns</a>
+            <a href="" className={styles.footerLinkBottom}>Privacy</a>
+            <a href="" className={styles.footerLinkBottom}>Terms</a>
+            <a href="" className={styles.footerLinkBottom}>Careers</a>
+          </div>
         </div>
-    </div>
-    <div className={baseStyles.col3}>
-        <div className={styles.footerTitle}>Recipes</div>
-        <div className={styles.footerParagraph}>
-          Sign up to our newsletter and get exclusive. Seedlip news, updates and offers.
-        </div>
-        <div className={styles.footerLinkMain}>
-          Get Recipes
-        </div>
-    </div>
-    <div className={baseStyles.col3}>
-        <div className={styles.footerTitle}>Story</div>
-        <div className={styles.footerParagraph}>
-          Sign up to our newsletter and get exclusive. Seedlip news, updates and offers.
-        </div>
-        <div className={styles.footerLinkMain}>
-          Read
-        </div>
+      </div>
     </div>
   </div>
 )
--- src/components/footer/styles.module.css.d.ts
+++ src/components/footer/styles.module.css.d.ts
@@ -1,3 +1,6 @@
 export const footerTitle: string;
 export const footerParagraph: string;
 export const footerLinkMain: string;
+export const footerSocialIcon: string;
+export const footerBottomLinkContainer: string;
+export const footerLinkBottom: string;
