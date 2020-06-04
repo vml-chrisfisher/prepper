6aad095078625b857f99772fd739a8203218ca28
Chris Fisher <chris.fisher@vml.com>
2020-06-03 22:42:23 -0500

    Added parents vegetables.

--- src/pages/plants.tsx
+++ src/pages/plants.tsx
@@ -7,8 +7,39 @@ import { PlantsEdge, PlantsProps } from '../page-interfaces/plants'
 
 class PlantsIndex extends React.Component<PlantsProps> {
   render() {
+    /* eslint-disable  @typescript-eslint/no-var-requires */
+    /* eslint-disable  no-undef */
+    const contentful = require('contentful')
+    const contentfulConfig = require('../../.contentful.json')
+    /* eslint-enable  @typescript-eslint/no-var-requires */
+    /* eslint-enable  no-undef */
+    const client = contentful.createClient({
+      space: contentfulConfig.spaceId,
+      accessToken: contentfulConfig.accessToken,
+    })
+
+    const asset = client.getAsset('1ivQJbUcXJJUiHGiL9d4tN').then((asset: any) => console.log(asset.fields.file.url))
+
     const siteTitle: string = get(this, 'props.data.site.siteMetadata.title')
     const posts: PlantsEdge[] = get(this, 'props.data.allContentfulVegetable.edges')
+    const vegetablesByParent = [
+      {
+        type: 'Vegetables',
+        subplants: [],
+      },
+      {
+        type: 'Fruits',
+        subplants: [],
+      },
+      {
+        type: 'Herbs',
+        subplants: [],
+      },
+      {
+        type: 'Grains',
+        subplants: [],
+      },
+    ]
     const vegetablesInFamilies: { [id: string]: PlantsEdge[] } = {}
     posts.map((plant: PlantsEdge) => {
       if (vegetablesInFamilies[plant.node.parentVegetable.name]) {
@@ -18,11 +49,12 @@ class PlantsIndex extends React.Component<PlantsProps> {
         vegetablesInFamilies[plant.node.parentVegetable.name].push(plant)
       }
     })
+    console.log(vegetablesInFamilies)
     return (
       <Layout location={this.props.location}>
         <div style={{ background: '#fff', paddingTop: '250px', position: 'absolute', top: '0', width: '100%' }}>
           <Helmet title={siteTitle} />
-          <h1>Articles</h1>
+          <h1>Plants</h1>
           <div className="wrapper">
             <div>
               {Object.keys(vegetablesInFamilies).map((key, index) => {
@@ -30,7 +62,12 @@ class PlantsIndex extends React.Component<PlantsProps> {
                   <div key={index}>
                     <div>{key}</div>
                     {vegetablesInFamilies[key].map((plant: PlantsEdge, index) => {
-                      return <div key={index}>{plant.node.title}</div>
+                      return (
+                        <div key={index}>
+                          {plant.node.title}
+                          {plant.node.parentVegetable.contentfulid}
+                        </div>
+                      )
                     })}
                   </div>
                 )
@@ -59,6 +96,7 @@ export const pageQuery = graphql`
           slug
           title
           parentVegetable {
+            contentfulid
             name
           }
         }
