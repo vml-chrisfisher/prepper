03a6852470f7adba61aefdf41f94ad888fe58e28
Chris Fisher <chris.fisher@vml.com>
2020-07-09 15:12:29 -0500

    Fixing preconnect.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -46,9 +46,9 @@ const Layout = ({ location, children }: Props) => {
           rel="preconnect"
           href="https://fonts.googleapis.com/css?family=Nunito:300,600%7CPlayfair+Display&display=swap"
         ></link>
-        <link as="script" rel="preload" href="https://www.google-analytics.com"></link>
+        <link as="script" rel="preconnect" href="https://www.google-analytics.com"></link>
 
-        <script async="true" src="https://www.googletagmanager.com/gtag/js?id=UA-127393105-1"></script>
+        <script rel="preconnect" src="https://www.googletagmanager.com/gtag/js?id=UA-127393105-1"></script>
         <script>{gTag}</script>
         <meta name="msvalidate.01" content="07B97569BEA5E257BB3E40E122BF9908" />
         <meta name="p:domain_verify" content="1d4f38850b9eef24be5e9ee3b9f16617" />
