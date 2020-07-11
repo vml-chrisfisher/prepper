6c4bd4d03bd228ccfb47fe9b2db1027b0ef86a92
Chris Fisher <chris.fisher@vml.com>
2020-07-11 01:29:11 -0500

    Fixed FeatureContentRow.

--- src/components/featuredContentRow/index.tsx
+++ src/components/featuredContentRow/index.tsx
@@ -6,28 +6,22 @@ import FeatureContentRowDetail from './featuredContentRowDetail'
 import FeatureContentRowProps from './interface'
 
 const FeaturedContentRow = (props: FeatureContentRowProps) => {
-  const maxNumber = Math.floor(props.features.length + 1)
-  const detailPosition = Math.floor(Math.random() * (props.features.length - 0 + 1))
-  const items = []
-  let b = 0
-  for (let a = 0; a < maxNumber; a++) {
-    if (a === detailPosition) {
-      items.push(
-        <Col3Full key={`key${a}`} className="col3">
-          <FeatureContentRowDetail {...props.details} />
-        </Col3Full>,
-      )
-    } else {
-      items.push(
-        <Col3Full key={`key${a}`} className="col3">
-          <ArticleSummaryVertical {...props.features[b]} />
-        </Col3Full>,
-      )
-      b++
-    }
-  }
-
-  return <FeatureContainer className="row">{items}</FeatureContainer>
+  return (
+    <FeatureContainer className="row">
+      <Col3Full className="col3">
+        <ArticleSummaryVertical {...props.features[0]} />
+      </Col3Full>
+      <Col3Full className="col3">
+        <ArticleSummaryVertical {...props.features[1]} />
+      </Col3Full>
+      <Col3Full className="col3">
+        <ArticleSummaryVertical {...props.features[2]} />
+      </Col3Full>
+      <Col3Full className="col3">
+        <FeatureContentRowDetail {...props.details} />
+      </Col3Full>
+    </FeatureContainer>
+  )
 }
 
 const Col3Full = styled.div`
