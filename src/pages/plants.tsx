8ca724c8fc75ce2d3f777ad09d48d035283c65ff
Chris Fisher <chris.fisher@vml.com>
2020-07-11 00:59:49 -0500

    Fixed story picture.

--- src/pages/plants.tsx
+++ src/pages/plants.tsx
@@ -20,7 +20,7 @@ class PlantsIndex extends React.Component<PlantsProps> {
       accessToken: contentfulConfig.accessToken,
     })
 
-    const asset = client.getAsset('1vApipwsb7YV9oVqMRG6xz').then((asset: any) => console.log(asset.fields.file.url))
+    const asset = client.getAsset('5pHNDnKSUEali4feRGZOVY').then((asset: any) => console.log(asset.fields.file.url))
     const siteTitle: string = get(this, 'props.data.site.siteMetadata.title')
     const posts: PlantsEdge[] = get(this, 'props.data.allContentfulVegetable.edges')
     const vegetablesByParent = [
