5f12c85fa6bf0dcf435f4cb086e432356619d064
Chris Fisher <chris.fisher@vml.com>
2020-07-16 15:45:44 -0500

    Setting cross-origin.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -48,9 +48,10 @@ const Layout = ({ location, children }: Props) => {
         <link rel="preconnect" href="https://knifeandfish.s3.amazonaws.com"></link>
         <link
           rel="preconnect"
+          crossOrigin=""
           href="https://fonts.googleapis.com/css?family=Nunito:300,600%7CPlayfair+Display&display=swap"
         ></link>
-        <link as="script" rel="preconnect" href="https://www.google-analytics.com"></link>
+        <link as="script" rel="preconnect" crossOrigin="" href="https://www.google-analytics.com"></link>
 
         <script defer src="https://www.googletagmanager.com/gtag/js?id=UA-127393105-1"></script>
         <script>{gTag}</script>
