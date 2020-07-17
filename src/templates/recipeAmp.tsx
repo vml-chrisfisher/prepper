b599ef212c5c9ab96e21a572b8454a2a4d31942f
Chris Fisher <chrisfisher236@gmail.com>
2020-07-17 00:35:18 -0500

    Fixed noscript for amp.

--- src/templates/recipeAmp.tsx
+++ src/templates/recipeAmp.tsx
@@ -353,16 +353,16 @@ class RecipeAMPTemplate extends React.Component<RecipeProps> {
                 animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}
               }`}
             </style>
-            {/* <noscript>
-              <style amp-boilerplate>
-                {`body{
+            <noscript>
+              {`<style amp-boilerplate>
+                body{
                   -webkit-animation:none;
                   -moz-animation:none;
                   -ms-animation:none;
                   animation:none
-                }`}
-              </style>
-            </noscript> */}
+                }
+              </style>`}
+            </noscript>
 
             <html lang="en" dir="ltr" amp />
           </Helmet>
