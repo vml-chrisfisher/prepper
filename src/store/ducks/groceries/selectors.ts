7329e58355481372f7099d94e04a2b7c66d4db24
Chris Fisher <chrisfisher236@gmail.com>
2021-03-30 10:14:01 -0500

    Created selectors for Groceries.

--- src/store/ducks/groceries/selectors.ts
+++ src/store/ducks/groceries/selectors.ts
@@ -1,5 +1,15 @@
-export const getGroceriesData = (state: any) => state?.header || {}
+import { GroceriesPreferences, GroceryList, PreferredStore, Store } from './interfaces'
 
-export const getGroceries = (state: any) => {
-  return getGroceriesData(state).groceries
+export const getGroceriesData = (state: any): GroceriesPreferences => state?.header || ({} as GroceriesPreferences)
+
+export const getPreferredStores = (state: any): Array<PreferredStore> => {
+  return getGroceriesData(state).preferredStores
+}
+
+export const getCloseStores = (state: any): Array<Store> => {
+  return getGroceriesData(state).closeStores
+}
+
+export const getGroceries = (state: any): Array<GroceryList> => {
+  return getGroceriesData(state).groceriesList
 }
